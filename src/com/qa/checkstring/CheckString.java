98ec64562952971370fa04d33c986c2d5cf8649a
shivalasvegas <shiva.king@academytrainee.com>
2020-09-14 16:09:42 +0100

    SK - changed to .equals() for comparing two strings

--- src/com/qa/checkstring/CheckString.java
+++ src/com/qa/checkstring/CheckString.java
@@ -11,7 +11,7 @@ public class CheckString {
 	static void checkString(String myString) {
 		
 		String yourString = "Mine";
-		if (myString == yourString) {
+		if (yourString.equals(myString)) {
 			System.out.println("Our strings match!");
 		}else {
 			System.out.println("Our strings don't match!");
@@ -22,7 +22,7 @@ public class CheckString {
 static void checkTwoStrings(String myString1, String myString2) {
 		
 		
-		if (myString1 == myString2) {
+		if (myString1.equals(myString2)) {
 			System.out.println("Your strings match!");
 		}else {
 			System.out.println("Your strings don't match!");
